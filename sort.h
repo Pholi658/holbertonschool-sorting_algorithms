e7f3608d1b9d95df0ba06d7bd07dfe962e052cfa
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2025-11-10 12:10:49 +0200

    Add Selection sort algorithm and Big O notation files

--- sort.h
+++ sort.h
@@ -1,8 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stdio.h>
+
 #include <stddef.h>
-#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -11,7 +10,6 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-
 typedef struct listint_s
 {
 	const int n;
@@ -19,9 +17,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Sorting algorithm prototypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
+/* Provided printing helpers (will be linked during correction) */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-#endif
+#endif /* SORT_H */
+
